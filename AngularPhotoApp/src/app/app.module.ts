d741013472eb788ce22ff5334b658ff5caa3d795
johnvillar41 <johndanielvillar123@gmail.com>
2021-10-08 21:17:58 +0800

    Added components

--- AngularPhotoApp/src/app/app.module.ts
+++ AngularPhotoApp/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ImageListComponent } from './image-list/image-list.component';
+import { SearchBarComponent } from './search-bar/search-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ImageListComponent,
+    SearchBarComponent
   ],
   imports: [
     BrowserModule,
