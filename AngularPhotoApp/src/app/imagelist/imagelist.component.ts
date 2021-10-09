02a039f73f6773ee199256e5519437ccdb0e3cdb
johnvillar41 <johndanielvillar123@gmail.com>
2021-10-09 16:20:58 +0800

    Updated design on imeglist

--- AngularPhotoApp/src/app/imagelist/imagelist.component.ts
+++ AngularPhotoApp/src/app/imagelist/imagelist.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./imagelist.component.css']
 })
 export class ImagelistComponent implements OnInit {
-
+  public items: Array<number> = [1, 2, 3, 4, 5, 6 ,7 ,8, 9, 10];
   constructor() { }
 
   ngOnInit(): void {
