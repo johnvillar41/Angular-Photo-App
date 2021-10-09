4d19509b840f7894061892e204281705a9bd11b2
johnvillar41 <johndanielvillar123@gmail.com>
2021-10-09 13:56:11 +0800

    Updated Modules and Components

--- AngularPhotoApp/src/app/app.module.ts
+++ AngularPhotoApp/src/app/app.module.ts
@@ -7,17 +7,29 @@ import { ImageListComponent } from './image-list/image-list.component';
 import { SearchBarComponent } from './search-bar/search-bar.component';
 
 import { HttpClientModule } from '@angular/common/http';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
   declarations: [
     AppComponent,
     ImageListComponent,
-    SearchBarComponent
+    SearchBarComponent,
+    ToolbarComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    MatInputModule,
+    MatIconModule,
+    MatCardModule,
+    MatButtonModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
