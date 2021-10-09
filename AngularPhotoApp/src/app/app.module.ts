de245317c26497802949596123cdbd993e4d9b96
johnvillar41 <johndanielvillar123@gmail.com>
2021-10-09 15:20:41 +0800

    rewrote

--- AngularPhotoApp/src/app/app.module.ts
+++ AngularPhotoApp/src/app/app.module.ts
@@ -3,34 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ImageListComponent } from './image-list/image-list.component';
-import { SearchBarComponent } from './search-bar/search-bar.component';
-
-import { HttpClientModule } from '@angular/common/http';
-import { MatInputModule } from '@angular/material/input';
-import { MatIconModule } from '@angular/material/icon';
-import { MatCardModule } from '@angular/material/card';
-import { MatButtonModule } from '@angular/material/button';
-import { ToolbarComponent } from './toolbar/toolbar.component';
-import { MatToolbarModule } from '@angular/material/toolbar';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SearchbarComponent } from './searchbar/searchbar.component';
+import { ImagelistComponent } from './imagelist/imagelist.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ImageListComponent,
-    SearchBarComponent,
-    ToolbarComponent
+    NavbarComponent,
+    SearchbarComponent,
+    ImagelistComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule,
-    MatInputModule,
-    MatIconModule,
-    MatCardModule,
-    MatButtonModule,
-    MatToolbarModule,
     BrowserAnimationsModule
   ],
   providers: [],
